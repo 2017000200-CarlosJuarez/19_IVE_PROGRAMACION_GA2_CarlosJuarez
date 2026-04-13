937f1bbfd0087cdff36c76dba91aa0e32ccd954c
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-04-13 12:14:32 -0600

    Add files via upload

---
+++ Ejercicio3 clase visual.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Letra segun su calificacion");
+        Console.WriteLine("Ingrese su letra de calificacion");
+        char letra = char.Parse(Console.ReadLine().ToUpper());
+
+        switch (letra)
+        {
+            case 'A':
+                Console.WriteLine("Excelente");
+                break;
+            case 'B':
+                Console.WriteLine("bueno");
+                break;
+            case 'C':
+                Console.WriteLine("Regular");
+                break;
+            case 'D':
+                Console.WriteLine("deficiente");
+                break;
+            case 'F':
+                Console.WriteLine("Reprobado");
+                break;
+            default:
+                Console.WriteLine("Numero no valido");
+                break;
+        }
+    }
+}
