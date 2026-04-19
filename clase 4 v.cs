3464aacfffe05a2a4e3668d03d2796c2154c1722
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-04-18 19:55:12 -0600

    Add files via upload

---
+++ clase 4 v.cs
@@ -0,0 +1,28 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("menu");
+        int m;
+
+        Console.WriteLine("1=hamburguesa");
+        Console.WriteLine("2=pizza");
+        Console.WriteLine("3=pollo frito");
+        Console.WriteLine("4=ensalada");
+
+        Console.WriteLine("Escriba un numero del 1 al 4");
+        m = Convert.ToInt32(Console.ReadLine());
+
+        switch(m)
+        {
+            case 1: Console.WriteLine("usted a pedido un menu de hamburguesa");
+                break;
+            case 2: Console.WriteLine("usted a pedido un menu de pizza");
+                break;
+            case 3: Console.WriteLine("usted a pedido un menu de pollo frito");
+                break;
+            case 4: Console.WriteLine("usted a pedido un menu de ensalada");
+                break;
+        }
+    }
+}
