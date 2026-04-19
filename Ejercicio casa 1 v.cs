755632231e27ffce1c2068f5fb865f1df0a573ec
2017000200-CarlosJuarez <c.juarez.5bdb@gmail.com>
2026-04-18 20:39:53 -0600

    Add files via upload

---
+++ Ejercicio casa 1 v.cs
@@ -0,0 +1,42 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("trigonometria");
+        double a,b,c,ta,co,se,o;
+
+        Console.WriteLine("Ingrese su lado A");
+        a = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese su lado B");
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Ingrese su lado C");
+        c = Convert.ToInt32(Console.ReadLine());
+
+        double ta = a / b;
+        double co = b / c;
+        double se = a / c;
+
+        Console.WriteLine("1 - tangente");
+        ta = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("2 - coseco");
+        co = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("seno");
+        se = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Escriba el numero de la operacion que desea realizar);
+        o = Convert.ToInt32(Console.ReadLine());
+
+        switch (o)
+        {
+            case 1:
+                Console.WriteLine(ta);
+                break;
+            case 2:
+                Console.WriteLine(co);
+                break;
+            case 3:
+                Console.WriteLine(se);
+                break;
+        }
+    }
+}
